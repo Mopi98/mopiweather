c9707fa74e82137fd7bf53ed4417c26a323f6119
Alex Moraes Pinheiro Filho <75181247+Mopi98@users.noreply.github.com>
2022-11-23 14:38:25 -0300

    Add files via upload

---
+++ app/admin/src/view/template/loading.php
@@ -0,0 +1,6 @@
+<div class="app-loading show">
+    <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+    <strong>Carregando...</strong>
+</div>
